bad0c42419b3560712bc0200bac310d53bcd7024
LuisRFuentes54 <luisrf0540@gmail.com>
2021-02-08 21:07:52 -0400

    Arregla algunos problemas menores

--- src/Client.java
+++ src/Client.java
@@ -2,6 +2,7 @@ import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Client {
     private Client() {
@@ -106,16 +107,18 @@ public class Client {
                             option2 = keyboard.nextInt();
                             switch (option2) {
                                 case 1:
-                                    String[] accounts = stub.getAccounts(username);
-                                    for (int i = 0; i <= accounts.length - 1; i++)
-                                        if(accounts[i] != null)
-                                            System.out.println(i+1 + "- " + accounts[i]);
+                                    ArrayList<String> accounts = stub.getAccounts(username);
+                                    int i = 1;
+                                    for (String acc : accounts){
+                                        System.out.println(i + "- " + acc);
+                                        i++;
+                                    }
                                     System.out.print("Seleccione la cuenta a consultar: ");
                                     option2 = keyboard.nextInt() - 1;
-                                    String[] transactions = stub.getTransactions(accounts[option2]);
-                                    for (int j = 0; j < transactions.length - 1; j++)
-                                        if(transactions[j] != null)    
-                                            System.out.println(transactions[j]);
+                                    ArrayList<String> transactions = stub.getTransactions(accounts.get(option2));
+                                    for (String trans : transactions){  
+                                        System.out.println(trans);
+                                    }
                                     break;
                             
                                 default:
